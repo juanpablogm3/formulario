035a89bbf12e9b073694bb50e62c9ff8f59e7218
Juan Pablo Garcia Martinez <juanpablogm3@hotmail.com>
2024-04-01 23:50:47 -0300

    prueba env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const requestHandler = (req, res) => {
 
 const server = http.createServer(requestHandler);
 
-const port = process.env.PORT || 3000; // Usar el puerto definido en .env o 3000 por defecto
+const port = process.env.PORT// || 3000; // Usar el puerto definido en .env o 3000 por defecto
 server.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
